6c321049db8d87da5a097d97b56b91a3944fe3dd
mousermachine <niklas.moser@hof-university.de>
2025-05-14 12:58:33 +0200

    Fixed "Push" data

--- src/ringbuffer.h
+++ src/ringbuffer.h
@@ -15,23 +15,29 @@ struct common_buffer_data
 
  struct common_buffer_data initialize_buffer (void) {
     struct common_buffer_data b1;
-    b1.index_last_element = 0u;
-    b1.ringbuffer_index = 0u;
-    b1.ringbuffer;
-    memset(b1.ringbuffer,0u,sizeof(b1.ringbuffer));    
+    b1.index_last_element = 0u;   // Starting with zero; values on this position will be replaced
+    b1.ringbuffer_index = RINGBUFFER_SIZE-1;     // Initialized to last Element; needed for Moving average
+    memset(b1.ringbuffer,0u,sizeof(b1.ringbuffer));
+        
     return b1;
 }
 
-void push_data_to_buffer (int32_t data, common_buffer_data buffer_data){
-    buffer_data.ringbuffer[buffer_data.ringbuffer_index] = data;
-    buffer_data.ringbuffer_index++;
-    if (buffer_data.ringbuffer_index >= sizeof(buffer_data.ringbuffer)) {
-        buffer_data.ringbuffer_index = 0u;
+void push_data_to_buffer (int32_t data, common_buffer_data buffer){
+    buffer.ringbuffer[buffer.index_last_element] = data;
+    buffer.ringbuffer_index++;
+    buffer.index_last_element++;
+    if (buffer.ringbuffer_index >= (sizeof(buffer.ringbuffer)/sizeof(buffer.ringbuffer[0]))) {
+        buffer.ringbuffer_index = 0u;
+
+    }
+
+    if (buffer.index_last_element >= (sizeof(buffer.ringbuffer)/sizeof(buffer.ringbuffer[0]))) {
+        buffer.index_last_element = 0u;
 
     }
 }
 
-int32_t moving_average (int32_t ringbuffer[]) {
+int32_t moving_average (common_buffer_data buffer) {
     int32_t buffer_sum = 0;
     int32_t buffer_average = 0;
     for (int i=0;i=sizeof(ringbuffer);i++) {
