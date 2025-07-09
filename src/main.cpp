36f8c770d0aa3b2800b82e539483b371c1eafb9c
Xell24 <Jgesell@hof-university.de>
2025-07-09 14:17:43 +0200

    kommentiert für dokumentation und verständlichkeit

--- src/main.cpp
+++ src/main.cpp
@@ -10,38 +10,45 @@
 #include "Hardware_Config.h"
 #include <BarcodeReader.h>
 
-// Time intervals
+/// Time intervals
 constexpr unsigned long READ_INTERVAL_MS = 2;     // Interval between readings
 constexpr unsigned long INTERVAL_STOP_COND = 100;
 
-// Data struct to be sent
+/// Data struct to be sent
 data sensorData;
 
-// Incoming data from sensor in 16-bit format
+/// Incoming data from sensor in 16-bit format
 int16_t accelX, accelY, accelZ;
 int16_t gyroX, gyroY, gyroZ;
 
-// Other variable definition
-int32_t filteredAccelX, filteredAccelY, filteredAccelZ;
+///filtered data after moving average
+int32_t filteredAccelX, filteredAccelY, filteredAccelZ=0; 
+///calculated speed after integration
 int32_t filtered_data_velocity_x = 0;
+///calculated position after integration
 uint64_t filtered_data_pos_x = 0u;
+///counter for cycles before sending 
 uint8_t counter_sending = 0u;
+/// placeholder for debugging return types
 int32_t acc_complete_for_debugging = 0;
+/// counter for cycles of motor voltage =0;
 uint8_t counter_standstill = 0u;
+/// track section beeing read from barcode reader
 uint8_t barcode_value = 0u;
+/// speed beeing read from barcode reader
 uint32_t barcode_debug_velocity = 0u;
 
- //Ringbuffer defined in "ringbuffer.h"
+///initialization of ringbuffer for 3 axis acceleration and motor voltage defined in "ringbuffer.h"
 struct common_buffer_data Struct_Accel_X  = initialize_buffer();
 struct common_buffer_data Struct_Accel_Y  = initialize_buffer();
 struct common_buffer_data Struct_Accel_Z  = initialize_buffer();
 struct common_buffer_data Struct_Motor_Voltage = initialize_buffer();
 
-// Timestamps
+/// Timestamps
 unsigned long previousMillis = 0;
 uint32_t previousMillis_stop_cond = 0;
 
-// Barcode 
+/// Barcode 
 void EIC_Handler(void) {
     if (EIC->INTFLAG.reg & EIC_INTFLAG_EXTINT7) {
         EIC->INTFLAG.reg = EIC_INTFLAG_EXTINT7;  // Flag löschen
@@ -50,9 +57,9 @@ void EIC_Handler(void) {
 }
 
 barcodeConfig_t barcode_config = {
-    .pin = PIN_PA07,          // Pin where the barcode reader is connected to
-    .bitLength = 9489,    // Length in µm of 1 bit (sequence of black and white section)
-    .readingTimeout = 40000  // Timeout in µs for the reading process
+    .pin = PIN_PA07,          /// Pin where the barcode reader is connected to
+    .bitLength = 9489,    /// Length in µm of 1 bit (sequence of black and white section)
+    .readingTimeout = 40000  /// Timeout in µs for the reading process
 };
 
 
@@ -81,23 +88,24 @@ void loop()
 
     if (IMU.readAcceleration(accelX, accelY, accelZ)) 
     {
+      ///zero movement in x direction, filters noise; sets all values below the threshold to zero
       if (abs(accelX)<ZERO_MOVEMENT)
       {
         accelX =0u;
       }     
-      // zero movemnet auch für y
+      /// zero movement auch für y
       if (abs(accelY)<ZERO_MOVEMENT)
       {
         accelY =0u;
       }     
 
-      // Aktualisierung des Ringpuffers 
+      /// putting the actual values to ringbuffer 
       push_data_to_buffer(accelX, &Struct_Accel_X);
       push_data_to_buffer(accelY, &Struct_Accel_Y);
       push_data_to_buffer(accelZ, &Struct_Accel_Z);
-
       push_data_to_buffer(analogRead(Motor), &Struct_Motor_Voltage);
-      // Auslesen der Filterwerte
+
+      ///filtering the values with moving average
       filteredAccelX = moving_average(&Struct_Accel_X) ;
       filteredAccelY = moving_average(&Struct_Accel_Y) ;
       filteredAccelZ = moving_average(&Struct_Accel_Z) ;
@@ -121,52 +129,49 @@ void loop()
       }
       else
       {
-        //integration of speed out of filtered x and y acceleration
+        ///integration of speed out of filtered x and y acceleration
          integration_32bit(&Struct_Accel_X, &filtered_data_velocity_x, filteredAccelX,filteredAccelY);
       }
       
     
-      //integration of position from intgrated speed; incrementation of send counter
+      ///integration of position from intgrated speed; incrementation of send counter
        integration_64bit(Struct_Accel_X, &filtered_data_pos_x, filtered_data_velocity_x);
        counter_sending++;
     }
 
 
-//reading of Gyroscope values and store them in gyroX,gyroY,gyroZ
+///reading of Gyroscope values and store them in gyroX,gyroY,gyroZ
     if (IMU.readGyroscope(gyroX, gyroY, gyroZ)) 
     {
     
-
-
-      // Evtl. auch hier Aktualierung des Ringpuffers für die Gyro-Werte
     }
       
 
-  // Barcode recognition
+  /// Barcode recognition
     barcode_error_t error = barcode_get(barcode_value, barcode_debug_velocity);
     if (error == READING_SUCCESSFUL){
-      // set back position to zero when entering a new track section
+      /// set back position to zero when entering a new track section
       filtered_data_pos_x = 0;
-      //take the velocity from the barcode reader to prevent integration problems
+      ///take the velocity from the barcode reader to prevent integration problems
       filtered_data_velocity_x=(int32_t)(barcode_debug_velocity*SPEED_SCALER);
 
     }
-    // after 10 programcycles actual values are send via udp to the mqtt bridge
+    /// after 10 programcycles actual values are send via udp to the mqtt bridge
 if (counter_sending>=10) 
 {
 
-    sensorData.accel_vec[0] = accelX;  //unfiltered acceleration x
-    sensorData.accel_vec[1] = accelY;  //unfiltered acceleration y
-    sensorData.accel_vec[2] = accelZ;  //unfiltered acceleration z
-    sensorData.gyro_vec[0] = gyroX;    //unfiltered gyro x
-    sensorData.gyro_vec[1] = gyroY;    //unfiltered gyro y
-    sensorData.gyro_vec[2] = gyroZ;    //unfiltered gyro z
-    sensorData.error_code = error; // 0 as standard --> machts was ihr wollt damit
+    sensorData.accel_vec[0] = accelX;  ///unfiltered acceleration x
+    sensorData.accel_vec[1] = accelY;  ///unfiltered acceleration y
+    sensorData.accel_vec[2] = accelZ;  ///unfiltered acceleration z
+    sensorData.gyro_vec[0] = gyroX;    ///unfiltered gyro x
+    sensorData.gyro_vec[1] = gyroY;    ///unfiltered gyro y
+    sensorData.gyro_vec[2] = gyroZ;    ///unfiltered gyro z
+    sensorData.error_code = error; /// 0 as standard --> machts was ihr wollt damit
 
 
-    sensorData.accel_lin = filteredAccelX;   //acceleration linear 
-    sensorData.speed_lin = filtered_data_velocity_x/SPEED_SCALER;    //speed linear in mm/s
-    sensorData.pos_lin = (uint32_t)(filtered_data_pos_x/POSITION_SCALER); // account for Integration error
+    sensorData.accel_lin = filteredAccelX;   ///acceleration linear 
+    sensorData.speed_lin = filtered_data_velocity_x/SPEED_SCALER;    ///speed linear in mm/s
+    sensorData.pos_lin = (uint32_t)(filtered_data_pos_x/POSITION_SCALER); /// account for Integration error
 
     sensorData.track_section = barcode_value;
    
--- src/ringbuffer.h
+++ src/ringbuffer.h
@@ -1,53 +1,83 @@
+/**
+ * @file ringbuffer.h
+ * @brief Beschreibung
+ * @author Mosi
+ * @date 
+ */
+
+
 #include <stdint.h>
 #include "SimpleNET.h"
 #include <Arduino.h>
 #include <stdlib.h>
 
 
-// Define length of buffer
+
+/// Define length of ringbuffer
 #define RINGBUFFER_SIZE 34u
-// Define Zero-Border for Acceleration values in x
+/// Define Zero-Border for Acceleration values in x and y, lower values will be set to 0
 #define ZERO_MOVEMENT 380u
-// define Scaler
+/// define Scaler calculation example is in documentation
 #define SPEED_SCALER 14196
 #define POSITION_SCALER 14196126u
-// define Zero-Border for contemplation of accel_y
+/// define Zero-Border for contemplation of accel_y
 #define ZERO_MOVEMENT_Y 40000u
-// define empiric factor for valueing accel_y in  turns
+/// define empiric factor for valueing accel_y in  turns
 #define k     20000000u
-// define a amount of samples before a standstill is calculated
+/// define a amount of samples of motor voltage before a standstill is calculated
 #define STANDSTILL  20u
 
 
 struct common_buffer_data
 {
     /* data */
-    int16_t ringbuffer[RINGBUFFER_SIZE];      //Buffer for sensor Values
-    uint8_t ringbuffer_index;                 //ringbuffer index with actual value
-    uint8_t index_last_element;               //index with element which is kicked in next call
-    int16_t kicked_value;                     //kicked element is stored here for moving average
-    int32_t buffer_sum;                       //sum in moving average filter
-    uint32_t last_time;                       //needed for integration
-    uint32_t current_time;                    //needed for Integration
-    int32_t merker_buffer_sum;                //needed for calculation of dx in 32 bit integration
-    int32_t merker_speed;                     //needed for calculation of dx in 64 bit integration
-    int32_t merker_accel_complete;            //needed for calculation of dx when acc_y is relevant
-    int32_t acc_complete;                     // placeholder in 32 bit integration for curve speed
+    ///Buffer for acceleration data
+    int16_t ringbuffer[RINGBUFFER_SIZE];      
+    ///ringbuffer index with actual value
+    uint8_t ringbuffer_index;                 
+    ///index of element which is kicked out of buffer in next call
+    uint8_t index_last_element;               
+    ///kicked element is stored here for moving average filter
+    int16_t kicked_value; 
+    ///sum in moving average filter                   
+    int32_t buffer_sum; 
+    ///needed for timestamp in integration method                     
+    uint32_t last_time;   
+    ///needed for timestamp in integration method 
+    uint32_t current_time;      
+    ///needed for calculation of data points in 32 bit integration             
+    int32_t merker_buffer_sum; 
+    ///needed for calculation of data points in 64 bit integration              
+    int32_t merker_speed;     
+    ///needed for calculation of data points when acc_y is relevant                
+    int32_t merker_accel_complete;   
+    /// placeholder in 32 bit integration when the car drives through a curve         
+    int32_t acc_complete;                     
 };
 
-
+///function for buffer creation an initalization
 struct common_buffer_data initialize_buffer (void);
 
-
+///function for storing values in the ringbuffer
+///@param data acceleration 
+///@param buffer pointer to struct for one direction
 void push_data_to_buffer (int32_t data,struct common_buffer_data* buffer);
 
-
+///function to apply a moving average filter on a ringpuffer
+///@param buffer pointer to struct of acceleration
 int32_t moving_average (struct common_buffer_data* buffer); 
 
-
+///function to integrate speed out of acceleration
+///@param buffer to struct of acceleration x
+///@param speed pointer to speed value
+///@param accel_linear acceleration in x
+///@param accel_Y acceleration in y 
 void integration_32bit(struct common_buffer_data* buffer,int32_t* speed, int32_t accel_linear,int32_t accel_Y); 
 
-
+///function to integrate position out of speed
+///@param buffer struct of accel_X
+///@param speed pointer to position 
+///@param speed_linear from integration
 void integration_64bit(struct common_buffer_data buffer,uint64_t * position, int32_t speed_linear); 
 
 
